1ae981d1966a6268d7a01acb7887a6871e824759
minji-kim525 <alswlwkd20@naver.com>
2022-06-27 20:22:03 +0900

    Initial commit

---
+++ src/main/java/com/sparta/webrtc/WebRtcApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.webrtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRtcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebRtcApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/sparta/webrtc/repository/AccountRepository.java
@@ -0,0 +1,2 @@
+package com.sparta.webrtc.repository;
+
